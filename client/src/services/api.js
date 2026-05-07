6e48f1a0b7800ca6e492f9eab6abc75088802070
Sushanth <pvsushanthpv@gmail.com>
2026-05-07 10:34:46 +0530

    Fixed Api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -7,4 +7,20 @@ const API = axios.create({
 
 });
 
+
+// TOKEN INTERCEPTOR
+API.interceptors.request.use((req) => {
+
+  const token =
+    localStorage.getItem("token");
+
+  if (token) {
+
+    req.headers.Authorization =
+      `Bearer ${token}`;
+  }
+
+  return req;
+});
+
 export default API;
